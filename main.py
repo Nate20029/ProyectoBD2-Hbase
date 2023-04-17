f1c72be9fbf8206eb5ec15c9507705f9be312263
Natanael Girón <70552363+Nate20029@users.noreply.github.com>
2023-04-17 03:11:26 -0600

    describe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random
+
 # Función para cerrar la ventana
 def salir():
     ventana.destroy()
@@ -16,8 +18,10 @@ def ventana_put():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="put", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 
 # Función para crear la nueva ventana
@@ -31,8 +35,10 @@ def ventana_get():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_scan():
@@ -45,8 +51,10 @@ def ventana_scan():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_delete():
@@ -58,9 +66,11 @@ def ventana_delete():
     ventana2.title("Funcion Delete")
 
     ventana2.geometry("500x300")
-    # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+   # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_count():
@@ -73,8 +83,10 @@ def ventana_count():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_truncate():
@@ -87,20 +99,29 @@ def ventana_truncate():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()                
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)                
 
 
 # Función para crear la nueva ventana
 def ventana_create():
+    last_row_key = 1
 
         # Definimos una función para crear la tabla
     def create_table(table_name, column_families):
         table = {}
+        last_row_key = 0
         table['name'] = table_name
         table['families'] = {}
-        for family in column_families:
-            table['families'][family] = {}
+        for family_name in column_families:
+            family = {}
+            family['row_key'] = last_row_key + 1
+            family['Timestamp'] = random.randint(10**9, 10**10-1)
+            family['value'] = f'val_{last_row_key+1}'
+            last_row_key += 1
+            table['families'][family_name] = family
         return table
 
     # Definimos una función que se ejecutará cuando el usuario presione el botón "Crear"
@@ -300,7 +321,7 @@ def ventana_disable_enable():
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
     boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    x = 460
+    x = 390
     y = 265
     boton_volver.place(x=x, y=y)
 
@@ -442,8 +463,10 @@ def ventana_alter():
 
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_drop():
@@ -454,10 +477,42 @@ def ventana_drop():
     ventana2 = tk.Toplevel()
     ventana2.title("Funcion drop")
 
+    def drop_table(table_name):
+        for i, table in enumerate(tables_enabled):
+            if table['name'] == table_name:
+                del tables_enabled[i]
+                print(f"La tabla {table_name} ha sido eliminada con éxito.")
+                return True
+        print(f"No se encontró la tabla {table_name}.")
+        return False
+    
+    def drop_all_tables():
+        tables_enabled.clear()
+        print("Todas las tablas han sido eliminadas con éxito.")
+    
+    # Crear los widgets
+    table_name_label = tk.Label(ventana2, text="Nombre de la tabla:")
+    table_name_entry = tk.Entry(ventana2)
+    drop_table_button = tk.Button(ventana2, text="Eliminar tabla", command=lambda: drop_table(table_name_entry.get()))
+    message_label = tk.Label(ventana2, text="")
+    drop_alltable_button = tk.Button(ventana2, text="Eliminar todas las tablas", command=drop_all_tables)
+    message_alllabel = tk.Label(ventana2, text="")
+    
+    # Posicionar los widgets en la ventana
+    table_name_label.pack()
+    table_name_entry.pack()
+    drop_table_button.pack()
+    message_label.pack()
+    drop_alltable_button.pack()
+    message_alllabel.pack()
+
     ventana2.geometry("500x300")
+    
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Función para crear la nueva ventana
 def ventana_describe():
@@ -468,10 +523,65 @@ def ventana_describe():
     ventana2 = tk.Toplevel()
     ventana2.title("Funcion Describe")
 
+        # función para simular el comando describe
+    def describe_table(table_name):
+        table_data = None
+        table_type = None
+
+        # buscar la tabla en la lista de habilitadas
+        for table in tables_enabled:
+            if table['name'] == table_name:
+                table_data = table
+                table_type = "ENABLE"
+                break
+
+        # buscar la tabla en la lista de deshabilitadas
+        if not table_data:
+            for table in tables_disabled:
+                if table['name'] == table_name:
+                    table_data = table
+                    table_type = "DISABLE"
+                    break
+
+        # si la tabla no se encuentra en ninguna lista
+        if not table_data:
+            return f"Table {table_name} not found\n"
+
+        # generar la salida para la tabla encontrada
+        table_text = f"Table  {table_name} is {table_type}\n"
+        table_text += f"{table_name}\n"
+        for family_name, family_options in table_data['families'].items():
+            table_text += f"\nNAME => {family_name}, "
+            for option_name, option_value in family_options.items():
+                table_text += f"{option_name} => {option_value}, "
+        return table_text
+
+    def show_table_info():
+        table_name = table_name_entry.get()
+        table_info = describe_table(table_name)
+        result_label.config(text=table_info)
+
+
+    # agregar un campo de texto para ingresar el nombre de la tabla
+    table_name_label = tk.Label(ventana2, text="Table Name")
+    table_name_label.pack()
+    table_name_entry = tk.Entry(ventana2)
+    table_name_entry.pack()
+
+    # agregar un botón para simular el comando describe
+    describe_button = tk.Button(ventana2, text="Describe", command=show_table_info)
+    describe_button.pack()
+
+    # agregar una etiqueta para mostrar el resultado de describe
+    result_label = tk.Label(ventana2, text="")
+    result_label.pack()
+
     ventana2.geometry("500x300")
     # Agregar un botón a la nueva ventana que cierre la ventana actual y muestre la ventana principal de nuevo
-    boton_volver = tk.Button(ventana2, text="Hola", command=lambda:[ventana2.destroy(), ventana.deiconify()])
-    boton_volver.pack()
+    boton_volver = tk.Button(ventana2, text="Regresar a Menú", command=lambda:[ventana2.destroy(), ventana.deiconify()])
+    x = 390
+    y = 265
+    boton_volver.place(x=x, y=y)
 
 # Crear la ventana principal
 ventana = tk.Tk()
